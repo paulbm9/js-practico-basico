94a4f5dddd4caad6793d2637248c984a434f7773
m9 <paulbravomacedo@gmail.com>
2022-05-20 23:00:54 -0500

    Reto 2 finalizado

---
+++ retos/retotaller2.js
@@ -0,0 +1,56 @@
+// const precioOriginal = 120;
+// const descuento = 18;
+//anadiendo cupones en la website.
+// const cupon = "treinta";
+var cupones = { treinta: 30, navidadya: 50, top40: 40 };
+
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+  return precioConDescuento;
+}
+
+function calcularDescuentoCupon() {
+  let descuento = 0;
+  const inputCupon = document.getElementById("input-cupon");
+  const cuponValue = inputCupon.value;
+  //   if (cuponValue === cupon) {
+  //     descuento = 30;
+  //     console.log(descuento);
+  //   } else {
+  //     descuento = 0;
+  //     console.log(descuento);
+  //   }
+  for (var c in cupones) {
+    if (cuponValue === c) {
+      descuento = cupones[c];
+      console.log(descuento);
+    }
+  }
+  return descuento;
+}
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("input-price");
+  const priceValue = inputPrice.value;
+  const inputDiscount = document.getElementById("input-discount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+  const precioConCupon = calcularPrecioConDescuento(
+    precioConDescuento,
+    calcularDescuentoCupon()
+  );
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio con descuento son: $" + precioConCupon;
+}
+// console.log({
+//   precioOriginal,
+//   descuento,
+//   porcentajePrecioConDescuento,
+//   precioConDescuento,
+// });
